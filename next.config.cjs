69dd8819e329627e82944db2e4aa2f15bbd101e9
Celo Composer <support@celo.org>
2026-05-19 23:18:31 +0100

    build: resolve and mock out react-native-async-storage and pino-pretty to fix vercel build

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,27 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+  webpack: (config, { webpack }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /@react-native-async-storage\/async-storage/,
+        path.resolve(__dirname, 'src/lib/mock-stub.js')
+      )
+    );
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /pino-pretty/,
+        path.resolve(__dirname, 'src/lib/mock-stub.js')
+      )
+    );
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
---
+++ src/lib/mock-stub.js
@@ -0,0 +1,2 @@
+// Mock stub file for Webpack resolution of native/node-only modules in browser bundle
+module.exports = {};
